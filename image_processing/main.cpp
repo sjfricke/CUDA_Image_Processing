da023c9fc94e8bb1dcd5437aa342c4929c8434eb
sjfricke <sjfricke@wisc.edu>
2017-02-11 02:03:55 -0600

    moved image data buffer

--- image_processing/main.cpp
+++ image_processing/main.cpp
@@ -11,6 +11,7 @@
 
 using namespace std;
 
+// used to save file of a buffer of Pixels
 int saveFile(Pixel* image_data, int height, int width, string output_file) {
 
   cv::Mat image_buffer = cv::Mat(height, width, CV_8UC3, (uint8_t*)image_data ).clone(); // make a copy
@@ -19,6 +20,48 @@ int saveFile(Pixel* image_data, int height, int width, string output_file) {
   cv::imwrite( output_file, image_buffer );
 
   debug("image write", "");
+  return 0;
+}
+
+/*
+ * used to store set of images by images in buffer
+ * image_1 = A; image_2 = B; both 16x8 deminsions
+ * [ A_x1_y1, A_x2_y1, A_x3_y1, ... , B_x15_y16, B_x16_y16 ]
+ */
+int bufferByImage(vector<cv::Mat>& IMAGE_FILES, Pixel* IMAGE_DATA, int image_count) {
+  
+  int i,j,k; // loops
+  Pixel pixel_buffer; // Pixel is a 3-byte struct for 8-bit RGB value
+  cv::Vec3b image_buffer;
+  uint64_t buffer_count = 0;
+  
+  // triple for loop... its works, I do what I want 
+  for ( i = 0; i < image_count; i++) {
+   
+    for( j = 0; j < IMAGE_FILES[i].rows; j++) {
+      for( k = 0; k < IMAGE_FILES[i].cols; k++) {
+
+	// i == image_index, j == row, k == column
+	
+        image_buffer = IMAGE_FILES[i].at<cv::Vec3b>(j,k);
+
+	// stores as BGR
+	// TODO, make sure Red and Blue are not swapped
+  	pixel_buffer.red = image_buffer[2];
+	pixel_buffer.green = image_buffer[1];
+	pixel_buffer.blue = image_buffer[0];
+	
+	// sets Pixel at spot in memory
+	// TODO - see if index math better then buffer count
+	*(IMAGE_DATA + buffer_count) = pixel_buffer;
+	
+	buffer_count++;
+      } //for cols
+    } // for rows
+  } // iterator end
+
+  debug("malloc pixels: ", buffer_count);
+
   return 0;
 }
 
@@ -36,13 +79,10 @@ int main(int argc, char **argv) {
     exit(1);
   }
   
-  int i,j,k; // loops
+  int i; // loops
   int err_check;
   //buffers to hold temp data
-  Pixel pixel_buffer;
-  cv::Vec3b image_buffer;
   cv::Mat mat_buffer;
-  uint64_t buffer_count = 0;
 
   // main data of images
   Pixel* IMAGE_DATA; //3D array of images
@@ -91,35 +131,8 @@ int main(int argc, char **argv) {
   }
 
   debug("byte size: ", (sizeof(Pixel) * image_width * image_height * image_count));
-  
-  // takes data from files to pixels
-  // triple for loop... its works, I do what I want 
-  for ( i = 0; i < image_count; i++) {
-   
-    for( j = 0; j < IMAGE_FILES[i].rows; j++) {
-      for( k = 0; k < IMAGE_FILES[i].cols; k++) {
 
-	// i == image_index, j == row, k == column
-	
-        image_buffer = IMAGE_FILES[i].at<cv::Vec3b>(j,k);
-
-	// stores as BGR
-	// TODO, make sure Red and Blue are not swapped
-  	pixel_buffer.red = image_buffer[2];
-	pixel_buffer.green = image_buffer[1];
-	pixel_buffer.blue = image_buffer[0];
-	
-	// sets Pixel at spot in memory
-	// TODO - see if index math better then buffer count
-	*(IMAGE_DATA + buffer_count) = pixel_buffer;
-	
-	buffer_count++;
-      }
-    }
-  } // iterator end
-  
-  debug("malloc pixels: ", buffer_count);
-
+  bufferByImage(IMAGE_FILES, IMAGE_DATA, image_count);
   
   output_file = "test_output.jpg";
 
